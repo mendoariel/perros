0f5c810229cacf6071213d57f4639c91086d752a
mendoariel <mendoariel@hotmail.com>
2025-02-04 12:59:01 -0300

    create two components add and my pets

---
+++ frontend/src/app/pages/add-pet/add-pet.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-add-pet',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './add-pet.component.html',
+  styleUrls: ['./add-pet.component.scss']
+})
+export class AddPetComponent {
+
+}
