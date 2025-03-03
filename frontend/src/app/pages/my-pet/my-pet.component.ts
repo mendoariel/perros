477cf93e02cfc456f066d8040482085e0eeee17d
Alberto Ariel Arce <albertoarielarce@MacBook-Air-de-Alberto.local>
2025-03-03 10:24:16 -0300

    advance into upload file frontend

--- frontend/src/app/pages/my-pet/my-pet.component.ts
+++ frontend/src/app/pages/my-pet/my-pet.component.ts
@@ -6,6 +6,7 @@ import { PetsService } from 'src/app/services/pets.services';
 import { AuthService } from 'src/app/auth/services/auth.service';
 import { MaterialModule } from 'src/app/material/material.module';
 import { FirstNavbarComponent } from 'src/app/shared/components/first-navbar/first-navbar.component';
+import { UploadFileService } from 'src/app/services/upload-file.service';
 
 @Component({
   selector: 'app-my-pet',
@@ -28,7 +29,8 @@ export class MyPetComponent implements OnInit, OnDestroy{
     private route: ActivatedRoute,
     private router: Router,
     private petsServices: PetsService,
-    private authService: AuthService
+    private authService: AuthService,
+    private uploadFileService: UploadFileService
   ) {}
   
   ngOnInit(): void {
@@ -47,6 +49,7 @@ export class MyPetComponent implements OnInit, OnDestroy{
     this.petsSubscription = this.petsServices.getMyPet(registerHash).subscribe({
       next: (myPet: any) => {
         this.myPet = myPet;
+        console.log('my pet ', this.myPet)
       },
       error: (error: any) => {
         console.error(error)
@@ -58,19 +61,37 @@ export class MyPetComponent implements OnInit, OnDestroy{
     this.router.navigate(['/mi-mascota', registerHash])
   }
 
-  onFileSelected() {
-    console.log('into file selected')
-    const inputNode: any = document.querySelector('#file');
-    let srcResult;
-    if (typeof (FileReader) !== 'undefined') {
-      const reader = new FileReader();
-  
-      reader.onload = (e: any) => {
-        console.log(srcResult = e.target.result)
-      };
-  
-      reader.readAsArrayBuffer(inputNode.files[0]);
+  onFileSelected(event: any) {
+    console.log('envent ', event)
+    if(event && event.target && event.target.files && event.target.files.length > 0) {
+      console.log('into here')
+      const file = event.target.files[0];
+      console.log(file)
+      const formData = new FormData();
+      formData.append('file', file);
+      formData.append('medalString', this.myPet.medals[0]);
+      console.log(formData)
+      this.uploadFileService.uploadProfileServie(formData).subscribe({
+        next: (res: any) => {
+          console.log(res)
+        },
+        error: (error: any) => {
+          console.log(error)
+        }
+      });
     }
+    // console.log('into file selected')
+    // const inputNode: any = document.querySelector('#file');
+    // let srcResult;
+    // if (typeof (FileReader) !== 'undefined') {
+    //   const reader = new FileReader();
+  
+    //   reader.onload = (e: any) => {
+    //     console.log(srcResult = e.target.result)
+    //   };
+  
+    //   reader.readAsArrayBuffer(inputNode.files[0]);
+    // }
   }
 
   ngOnDestroy(): void {
