d0ff56d7be4aa3e1393143618878033cd3175643
mendoariel <mendoariel@hotmail.com>
2025-03-10 15:12:27 -0300

    add qr generato to generate medas virgin

--- qr_generator/app.js
+++ qr_generator/app.js
@@ -1,24 +1,98 @@
 const fs = require('fs');
 
 const qr = require('qrcode');
+const { getClient } = require('./get-client');
 
-const data = 'https://peludosclick.com/mascota-checking?medalString=rosa_mosqueta';
+// const data = 'https://peludosclick.com/mascota-checking?medalString=rosa_mosqueta';
 // const data = 'https://peludosclick.com';
-const filePath = 'qr-rose.png';
+const filePath = 'qr-rose-scale-4.svg';
 
-qr.toFile(
-    filePath,
-    data,
-    {
-        errorCorrectionalLevel: 'H',
-        margin: 2,
-        scale: 4
-    },
-    err => {
-        if(err) {
-            console.log('Error generation')
-        } else {
-            console.log('code generated')
-        }
+let createHash = require('hash-generator');
+
+
+function insertVirginMedalRegister() {
+    (async () => {
+        const client = await getClient();
+        const medalString = createHash(36);
+    
+        let createQuery = `
+            INSERT INTO virgin_medals (status, medal_string, register_hash) 
+            VALUES ('VIRGIN', '${medalString}', 'genesis');
+        `;
+    
+        const res = await client.query(createQuery);
+        console.log('res from app ', res);
+        await client.end();
     }
-);
+    )();
+}
+
+// function to insert register into medal register
+// for (let index = 37; index > 0; index--) {
+//     insertVirginMedalRegister();
+// }
+
+
+var  qrMakeSvgFile = async function () {
+    // query to get all virgin medal
+    const client = await getClient();
+    let createQuery = `SELECT * FROM virgin_medals 
+        WHERE register_hash = 'genesis'`;
+    const medalToSvg = await client.query(createQuery);
+    client.end();
+    console.log('function MakeSvg ====> ', medalToSvg);
+
+    let fileId = 0;
+    medalToSvg.rows.map(medal => {
+
+        console.log('medal to make svg file ===> ', medal)
+        const data = `https://peludosclick.com/mascota-checking?medalString=${medal.medal_string}`;
+        const filePath = `medals_qr_images/2025-03-10-${++fileId}-sgv-qr-file-${medal.medal_string}.svg`
+        qr.toFile(
+            filePath,
+            data,
+            {
+                errorCorrectionalLevel: 'H',
+                margin: 2,
+                scale: 4,
+                type: "svg"
+            },
+            err => {
+                if(err) {
+                    console.log('Error generation')
+                } else {
+                    console.log('code generated')
+                }
+            }
+        );
+    });
+    
+
+
+}
+
+qrMakeSvgFile();
+
+// query to get all virgin medal
+
+// then loop the res, and make one file to each one medal that
+//      that resgister hash === 'genesis'
+
+
+// qr.toFile(
+//     filePath,
+//     data,
+//     {
+//         errorCorrectionalLevel: 'H',
+//         margin: 2,
+//         scale: 4,
+//         type: "svg"
+//     },
+//     err => {
+//         if(err) {
+//             console.log('Error generation')
+//         } else {
+//             console.log('code generated')
+//         }
+//     }
+// );
