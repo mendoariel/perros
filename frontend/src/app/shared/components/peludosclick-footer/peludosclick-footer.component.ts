a485bbc403c80714ac69cbdb505d5957007fd73b
mendoariel <mendoariel@hotmail.com>
2025-03-27 14:47:57 -0300

    working into wellcome component, put a footer, and change ohter thing into wellocme component

---
+++ frontend/src/app/shared/components/peludosclick-footer/peludosclick-footer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-peludosclick-footer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './peludosclick-footer.component.html',
+  styleUrls: ['./peludosclick-footer.component.scss']
+})
+export class PeludosclickFooterComponent {
+
+}
