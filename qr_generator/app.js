702385337d90802024413e4134cc1f4a8f619dc0
mendoariel <mendoariel@hotmail.com>
2025-05-08 13:44:29 -0300

    before create qr to resine

--- qr_generator/app.js
+++ qr_generator/app.js
@@ -6,51 +6,52 @@ const { getClient } = require('./get-client');
 let createHash = require('hash-generator');
 
 
-function insertVirginMedalRegister() {
+function insertVirginMedalRegister(howManyMedalsCreate) {
     (async () => {
         const client = await getClient();
-        const medalString = await createUniqueHash();
-    
-        let createQuery = `
-            INSERT INTO virgin_medals (status, medal_string, register_hash) 
-            VALUES ('VIRGIN', '${medalString}', 'third-round');
-        `;
+        
+        for (let index = howManyMedalsCreate; index > 0; index--) {
+            const medalString = await createUniqueHash(client);
+            let createQuery = `
+                INSERT INTO virgin_medals (status, medal_string, register_hash) 
+                VALUES ('VIRGIN', '${medalString}', 'first-round-ezequiel');
+            `;
     
         const res = await client.query(createQuery);
         console.log('res from app ', res);
-        await client.end();
+        }
+        await client.end()
+       
+        
     }
     )();
 }
 
-var createUniqueHash = async function () {
+var createUniqueHash = async function (client) {
     let medalString = createHash(36);
-    const client = await getClient();
+    //const client = await getClient();
     let createQuery = `SELECT * FROM virgin_medals  WHERE medal_string = '${medalString}'`;
     const medal = await client.query(createQuery);
-    client.end();
-    if(medal.rows.length>0) createUniqueHash();
+    if(medal.rows.length>0) createUniqueHash(client);
     else return medalString;
 }
 
+//insertVirginMedalRegister(200);
 
-//function to insert register into medal register
-// for (let index = 10; index > 0; index--) {
-//     insertVirginMedalRegister();
-// }
+
 
 
 var  qrMakeSvgFile = async function () {
     // query to get all virgin medal
     const client = await getClient();
     let createQuery = `SELECT * FROM virgin_medals 
-        WHERE register_hash = 'third-round'`;
+        WHERE register_hash = 'first-round-ezequiel'`;
     const medalToSvg = await client.query(createQuery);
     client.end();
 
     medalToSvg.rows.map(medal => {
-        const data = `https://peludosclick.com/mascota-checking?medalString=${medal.medal_string}`;
-        const filePath = `medals_qr_images/2025-04-30-${medal.id}-png-qr-file-${medal.medal_string}-${medal.register_hash}.png`
+        const data = `https://www.peludosclick.com/mascota-checking?medalString=${medal.medal_string}`;
+        const filePath = `medals_qr_images/ezequiel/first-round/2025-05-06-${medal.id}-png-qr-file-${medal.medal_string}-${medal.register_hash}.png`
         qr.toFile(
             filePath,
             data,
