ba8408164e40ce72924c86c87e75f5be036dff3a
Alberto Ariel Arce <albertoarielarce@MacBook-Air-de-Alberto.local>
2025-06-06 23:02:03 -0300

    trating to run like production

---
+++ backend-vlad/scripts/wait-for-db.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# wait-for-db.sh
+
+set -e
+
+echo "Generating Prisma Client..."
+npx prisma generate
+
+echo "Waiting for database..."
+until PGPASSWORD=casadesara psql -h postgres -U mendoariel -d peludosclick -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing migrations"
+npx prisma migrate deploy
+
+exec "$@" 
