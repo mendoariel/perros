0c3a0ee5ff05d2c22ecf7e430d2769b0106c5035
Ariel Arce <mendoariel@gmail.com>
2025-06-13 23:00:38 +0000

    fixing ssr 2

--- frontend/src/app/services/qr-checking.service.ts
+++ frontend/src/app/services/qr-checking.service.ts
@@ -1,12 +1,16 @@
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Injectable } from "@angular/core";
+import { Injectable, Inject, PLATFORM_ID } from "@angular/core";
 import { environment } from "src/environments/environment";
 import { ConfirmAccountInterface, ConfirmMedalInterface, MedalRegisterInterface } from "../interface/medals.interfae";
 import { ConfirmAccountComponent } from "../pages/confirm-account/confirm-account.component";
+import { isPlatformServer } from '@angular/common';
 
 @Injectable({providedIn: 'root'})
 export class QrChekingService {
-    constructor(private http: HttpClient){}
+    constructor(
+        private http: HttpClient,
+        @Inject(PLATFORM_ID) private platformId: Object
+    ){}
 
     private getHeaders() {
         const token = localStorage.getItem('access_token');
@@ -18,28 +22,35 @@ export class QrChekingService {
         return {}; // No headers if no token
     }
 
+    private getApiUrl() {
+        if (isPlatformServer(this.platformId)) {
+            return 'http://api.peludosclick.com/';
+        }
+        return environment.perrosQrApi;
+    }
+
     checkingQr(hash: string): any {
-        return this.http.post(`${environment.perrosQrApi}qr/checking`, {"medalString": hash});
+        return this.http.post(`${this.getApiUrl()}qr/checking`, {"medalString": hash});
     }
 
     medalRegister(registerObject: MedalRegisterInterface) {
-        return this.http.post(`${environment.perrosQrApi}qr/pet`, registerObject, this.getHeaders());
+        return this.http.post(`${this.getApiUrl()}qr/pet`, registerObject, this.getHeaders());
     }
 
     confirmAccount(confirmObject: ConfirmAccountInterface) {
-        return this.http.post(`${environment.perrosQrApi}auth/confirm-account`, confirmObject, this.getHeaders());
+        return this.http.post(`${this.getApiUrl()}auth/confirm-account`, confirmObject, this.getHeaders());
     }
 
     confirmMedal(confirmObject: ConfirmMedalInterface) {
-        return this.http.post(`${environment.perrosQrApi}auth/confirm-medal`, confirmObject, this.getHeaders());
+        return this.http.post(`${this.getApiUrl()}auth/confirm-medal`, confirmObject, this.getHeaders());
     }
 
     getPet(medalString: string): any {
-        return this.http.get(`${environment.perrosQrApi}qr/pet/${medalString}`)
+        return this.http.get(`${this.getApiUrl()}qr/pet/${medalString}`)
     }
 
     isThisEmailTaken(email: string): any {
-        return this.http.get(`${environment.perrosQrApi}qr/this-email-is-taken/${email}`);
+        return this.http.get(`${this.getApiUrl()}qr/this-email-is-taken/${email}`);
     }
 
     nextFunction() {
