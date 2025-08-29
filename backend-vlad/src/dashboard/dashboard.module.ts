242b9c1eb9739b8b19004f61e24f2d94be6d6696
mendoariel <mendoariel@hotmail.com>
2025-08-29 10:59:32 -0300

    fix: resolve merge conflict in dashboard module

--- backend-vlad/src/dashboard/dashboard.module.ts
+++ backend-vlad/src/dashboard/dashboard.module.ts
@@ -2,16 +2,10 @@ import { Module } from '@nestjs/common';
 import { DashboardController } from './dashboard.controller';
 import { DashboardService } from './dashboard.service';
 import { PrismaModule } from '../prisma/prisma.module';
-<<<<<<< HEAD
 import { MailModule } from '../mail/mail.module';
 
 @Module({
   imports: [PrismaModule, MailModule],
-=======
-
-@Module({
-  imports: [PrismaModule],
->>>>>>> gary
   controllers: [DashboardController],
   providers: [DashboardService],
   exports: [DashboardService]
